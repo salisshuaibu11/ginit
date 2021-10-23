7ee75a81b7497142922aa067591e4437f3ba62ed
salis <salisshuaibu11@gmail.com>
2021-10-23 08:27:10 +0000

    Created a function in inquirer file to ask for github credentials

--- index.js
+++ index.js
@@ -2,3 +2,22 @@ const chalk = require("chalk");
 const clear = require("clear");
 const figlet = require("figlet");
 const files = require("./lib/files");
+const inquirer = require("./lib/inquirer");
+
+console.log(
+    chalk.yellow(
+        figlet.textSync("Ginit", {horizontalLayout: "full"})
+    )
+);
+
+// if (files.directoryExists(".git")) {
+//     console.log(chalk.red("Already a git repository!"));
+//     process.exit();
+// };
+
+const run = async () => {
+    const credentials = await inquirer.askGithubCredentials();
+    console.log(credentials);
+};
+
+run();
